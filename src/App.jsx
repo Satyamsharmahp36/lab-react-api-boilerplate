5db937c2d9a24e3ef6b1dc4c447fcae0fa086543
Satyam <satyamworks20@gmail.com>
2024-02-02 22:40:02 +0530

    Styling done

--- src/App.jsx
+++ src/App.jsx
@@ -27,28 +27,25 @@ useEffect(()=>{
 return (
   <>
     <div>
-      {state.map((el)=>{
+      {state.map((e)=>{
+
+// giving diffent id to the exery div so it can be rendered again
+
         return (
-          <div key={el.id}>
-            <h2 style={{
-              textAlign:'left'
-            }} >{el.title}</h2>
-            <div style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems : 'center'
-            }}>
-            <img style={{
-              width: '13%'
-            }} src={el.imageLinks.thumbnail}/>
-            <p style={{
-              width:'80%',
-              textAlign: 'left'
-            }}>{el.description}</p>
+          <div key={e.id}>
+            <h2 style={{color:"orange", textAlign:'left' , fontSize:"2vw"}} >
+              {e.title}
+              </h2>
+
+            <div id='mainSection'>
+
+            <img src={e.imageLinks.thumbnail} alt="images" />
+
+            <p> {e.description} </p>
+
             </div>
-            <h4 style={{
-              textAlign:'left'
-            }}>{el.authors}</h4>
+
+            <h4>{e.authors}</h4>
             <hr />
           </div>
         )
